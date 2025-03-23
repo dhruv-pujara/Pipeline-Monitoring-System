9df8696f13e68f39161cc29b617ba56124bd4ddf
nebilawako <nebilawako@gmail.com>
2025-03-23 12:39:17 -0600

    login page front end done add dark theme

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,45 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-export default function Home() {
+export default function TabsDemo() {
   return (
-    <div>
-      <Button>Get to coding</Button>
+    <div className="flex h-screen items-center justify-center">
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsContent value="account">
+          <Card>
+            <CardHeader className="text-center">
+              <CardTitle>Welcome to PipeX!</CardTitle>
+              <CardDescription>
+              Real-time pipeline inspection and monitoring—
+Precision, safety, and control at your fingertips.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Username</Label>
+                <Input id="name"/>
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password"/>
+              </div>
+            </CardContent>
+            <CardFooter className="flex justify-center">
+              <Button >Log In</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
-  );
+  )
 }
